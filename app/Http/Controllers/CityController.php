c9df504b9c380ca62a8edf3ef1648da7c470df81
albertoanta <anta.alberto@gmail.com>
2022-04-19 16:10:02 +0200

    City Model and Controller

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\City;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+   
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $cities = City::get();
+    }
+
+   
+}
